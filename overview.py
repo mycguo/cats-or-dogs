4bff8fa2451bdb475491523e829b97af5366b4b7
charles <yinchao.guo@hpe.com>
2025-03-23 01:36:14 -0700

    add overview

---
+++ overview.py
@@ -0,0 +1,12 @@
+import streamlit as st
+
+st.title("Overview")
+st.write("This is adopted from https://github.com/fastai/fastbook/tree/master")
+
+
+def main():
+    st.header("List of Pages")
+    st.write("1. [Cats or Dogs](/pages/cats-or-dogs)")
+
+if __name__ == "__main__":
+    main()
